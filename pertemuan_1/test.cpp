a3f811d8e0021457dadc5d2a5265fb0430422bc8
MuhammadSyukronJazila-231401007 <151598792+MuhammadSyukronJazila-231401007@users.noreply.github.com>
2024-05-31 21:28:52 +0700

    Update test.cpp

--- pertemuan_1/test.cpp
+++ pertemuan_1/test.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
+#include <conio.h> // library untuk getch() dkk
 using namespace std;
 
 main(){
+    // char nama[50];
+    // int nim;
+    // char kom;
+    // float ip;
+
+    // printf("Hello World\n"); // menampilkan Hello World
+
+    // printf("Masukkan nama : ");
+    // gets(nama);     //  gets = get string( 1 kalimat aja)
+
+    // printf("Masukkan NIM : ");
+    // scanf("%d", &nim); // %d untuk input tipe data int
+
+    // printf("Masukkan KOM : ");
+    // scanf(" %c", &kom); // %c untuk input tipe data char
+
+    // printf("Masukkan IP : ");
+    // scanf("%f", &ip); // %f untuk input tipe data real
+
+    // printf("Nama : ");
+    // puts(nama);     //* puts = put string (cetak string)
+
+    // printf("NIM : %d\n", nim);
+    // printf("KOM : %c\n", kom);
+    // printf("IP : %.3f\n", ip);
+
+    // printf("Press any button to continue...");
+    // getch(); // klik tombol apapun untuk mengakhiri program
+    
     string nama;
     int nim;
     char kom;
